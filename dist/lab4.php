cd5c3f3e08d307661a172b77a537a0c332d66c9a
DimDimich-creator <stilys62@gmail.com>
2023-02-28 21:37:06 +0300

    добавил блок схему

--- dist/lab4.php
+++ dist/lab4.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/src/assets/home.svg" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
     <link rel="stylesheet" href="style.css" />
     <title>Лабораторная работа 4</title>
 </head>
@@ -69,6 +69,7 @@
             echo "Значение переменной x = $x";
             ?>
             </div>
+            <img src="block.svg" alt="" class='mt-10 ml-8'>
         </div>
     </main>
 
